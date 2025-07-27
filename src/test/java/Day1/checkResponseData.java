3064d92cf1cd0da47927024ad95c6a6db3bc88aa
Ashitosh Kole <ashitoshkole007@gmail.com>
2025-07-27 23:23:38 +0530

    added checkResponseData

--- src/test/java/Day1/checkResponseData.java
+++ src/test/java/Day1/checkResponseData.java
@@ -9,6 +9,8 @@ import io.restassured.response.Response;
 
 import java.util.HashMap;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -28,6 +30,35 @@ public class checkResponseData {
 	       Assert.assertEquals(res.statusCode(), 200);
 		
 		
+	}
+	
+	
+	@Test
+	public void checkJSONData() {
+		
+		Response res = given()
+				.when()
+				      .get("http://localhost:3000/students");
+		
+		System.out.println(res.asString());
+		
+		JSONArray jo = new JSONArray(res.asString());
+		
+		Boolean status = false;
+		
+		for(int i=0;i<=jo.length();i++) {
+			
+			System.out.println(jo.getJSONObject(i).get("name").toString());
+			String name = jo.getJSONObject(i).get("name").toString();
+			
+			if(name.equalsIgnoreCase("Ashitosh")) {
+				status = true;
+				break;
+			}
+		}
+		
+		Assert.assertEquals(status,true);
+				      
 	}
 
 }
